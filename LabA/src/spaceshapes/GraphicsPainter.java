69bb3912e764fb23750caccc1ff97c61142e62de
Alex Verkerk <alxjlv@gmail.com>
2019-05-01 16:37:05 +1200

    Added project files

---
+++ LabA/src/spaceshapes/GraphicsPainter.java
@@ -0,0 +1,45 @@
+package spaceshapes;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+/**
+ * Implementation of the Painter interface that delegates drawing to a
+ * java.awt.Graphics object.
+ * 
+ * @author Paramvir Singh (Original Author - Ian Warren)
+ * 
+ */
+public class GraphicsPainter implements Painter {
+	// Delegate object.
+	private Graphics _g;
+
+	/**
+	 * Creates a GraphicsPainter object and sets its Graphics delegate.
+	 */
+	public GraphicsPainter(Graphics g) {
+		this._g = g;
+		_g.setColor(new Color(212, 212, 212));
+	}
+
+	/**
+	 * @see spaceshapes.Painter.drawRect
+	 */
+	public void drawRect(int x, int y, int width, int height) {
+		_g.drawRect(x, y, width, height);
+	}
+
+	/**
+	 * @see spaceshapes.Painter.drawOval
+	 */
+	public void drawOval(int x, int y, int width, int height) {
+		_g.drawOval(x, y, width, height);
+	}
+
+	/**
+	 * @see spaeshapes.Painter.drawLine.
+	 */
+	public void drawLine(int x1, int y1, int x2, int y2) {
+		_g.drawLine(x1, y1, x2, y2);
+	}
+}
