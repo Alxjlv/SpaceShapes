7258ba15381c25e1a232c6fe3fe2124b60c08a6d
Alex Verkerk <alxjlv@gmail.com>
2019-05-20 17:18:29 +1200

    Created carriershape

---
+++ LabA/src/spaceshapes/CarrierShape.java
@@ -0,0 +1,34 @@
+package spaceshapes;
+
+public class CarrierShape extends Shape {
+
+	public CarrierShape() {
+		super();
+	}
+
+	public CarrierShape(int x, int y) {
+		super(x, y);
+	}
+
+	public CarrierShape(int x, int y, int deltaX, int deltaY) {
+		super(x, y, deltaX, deltaY);
+	}
+
+	public CarrierShape(int x, int y, int deltaX, int deltaY, int width, int height) {
+		super(x, y, deltaX, deltaY, width, height);
+	}
+
+	@Override
+	public void paint(Painter painter) {
+		painter.drawRect(_x,_y,_width,_height);
+	}
+
+	@Override
+	public void move(int width, int height) {
+		
+	}
+	
+	void add(Shape shape) {
+		
+	}
+}
